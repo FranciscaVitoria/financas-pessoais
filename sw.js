b93bbe5da111dd0b64ce1e6d4680a0b9e60e06f2
FranciscaVitoria <103030510+FranciscaVitoria@users.noreply.github.com>
2026-08-30 20:42:59 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'financas-pessoais-v23';
+const CACHE = 'financas-pessoais-v25';
 const ASSETS = ['./','index.html','styles.css','app.js','manifest.webmanifest','icons/icon-192.png','icons/icon-512.png'];
 
 self.addEventListener('install', event => {
